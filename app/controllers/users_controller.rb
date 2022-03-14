a719c73a6e3b0671ada096121c737c9c48ebacb5
binarygit <inscription4@outlook.com>
2022-03-14 18:41:24 +0545

    add new and create actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(new_user_parmas)
+
+    if @user.save
+      flash.notice = 'Please check your email for sign up confirmation'
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 end
