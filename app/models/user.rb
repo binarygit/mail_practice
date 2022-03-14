f0bf533986473f365f9ed283524ec25ae9594bfa
binarygit <inscription4@outlook.com>
2022-03-14 18:31:22 +0545

    prepare full_name before creating User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   validates :password, confirmation: true
   validates :username, uniqueness: true
   validates :last_name, :first_name, :password_confirmation, presence: true
+
+  before_create :prepare_full_name
+
+  private
+
+  def prepare_full_name
+    if self.middle_name.nil? 
+      self.first_name + ' ' + self.last_name
+    else
+      self.first_name + ' ' + self.middle_name + ' ' + self.last_name
+    end
+  end
 end
