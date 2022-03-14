6cd423e697802238e3a45ed569a8aab0d4c254e1
binarygit <inscription4@outlook.com>
2022-03-14 18:11:51 +0545

    add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :password, confirmation: true
+  validates :username, uniqueness: true
+  validates :last_name, :first_name, :password_confirmation, presence: true
 end
